2e464dfa378de34331a14c538d5d7b9efa04c8d0
AbdelrahmanMosly <95633556+AbdelrahmanMosly@users.noreply.github.com>
2024-05-15 01:07:24 +0300

    logging added

--- GraphClient.java
+++ GraphClient.java
@@ -2,10 +2,17 @@ import java.io.*;
 import java.rmi.Naming;
 import java.util.Objects;
 import java.util.Random;
-
+import java.util.logging.*;
 public class GraphClient {
+    private static final Logger logger = Logger.getLogger(GraphClient.class.getName());
+
     public static void main(String[] args) {
         try {
+            Handler fileHandler = new FileHandler("GraphClient.log");
+            fileHandler.setFormatter(new SimpleFormatter());
+            logger.addHandler(fileHandler);
+            logger.setLevel(Level.INFO);
+
             Configuration config = new Configuration();
             GraphService graphService = (GraphService) Naming.lookup("//" + config.getServerAddress() + ":" + config.getRmiregistryPort() + "/GraphService");
 
@@ -15,7 +22,7 @@ public class GraphClient {
 
             // Generate random instructions and save them in a file
             String instructionsFile = "instructions.txt";
-            generateRandomInstructions(instructionsFile);
+//            generateRandomInstructions(instructionsFile);
 
             // Read instructions from the file and send them to the server
             // Measure performance and log results
@@ -23,39 +30,47 @@ public class GraphClient {
             // Repeat the process
             while (true) {
                 long startTime = System.currentTimeMillis();
-                //Read instructions from file and send to server
-                try (BufferedReader reader = new BufferedReader(new FileReader(instructionsFile))) {
+                //Read instructions from file and send to server and measure performance and log results in GrpahClient.log
+                try (BufferedReader reader = new BufferedReader(new FileReader(instructionsFile)) ) {
                     String line;
                     while (!Objects.equals(line = reader.readLine(), "F")) {
                         String[] tokens = line.split(" ");
                         char operation = tokens[0].charAt(0);
                         int src = Integer.parseInt(tokens[1]);
                         int dest = Integer.parseInt(tokens[2]);
+                        long operationStartTime = System.currentTimeMillis();
+
                         switch (operation) {
                             case 'A':
+                                logger.info("Adding edge: " + src + " -> " + dest);
                                 graphService.addEdge(src, dest);
+                                logger.info("Added edge: " + src + " -> " + dest);
+                                logger.info("Finished in  " + (System.currentTimeMillis()-operationStartTime) + " milliseconds");
+
                                 break;
                             case 'D':
+                                logger.info("Removing edge: " + src + " -> " + dest);
                                 graphService.removeEdge(src, dest);
+                                logger.info("Removed edge: " + src + " -> " + dest);
+                                logger.info("Finished in  " + (System.currentTimeMillis()-operationStartTime) + " milliseconds");
                                 break;
                             case 'Q':
+                                logger.info("Querying shortest path from " + src + " to " + dest);
                                 int shortestPath = graphService.shortestPath(src, dest);
-                                System.out.println("Shortest path from " + src + " to " + dest + ": " + shortestPath);
+                                logger.info("Shortest path from " + src + " to " + dest + ": " + shortestPath);
+                                logger.info("Finished in  " + (System.currentTimeMillis()-operationStartTime) + " milliseconds");
                                 break;
                             default:
-                                System.out.println("Invalid operation: " + operation);
+                                logger.warning("Invalid operation: " + operation);
                         }
                     }
                 }
 
-                // TODO: Log results
-
                 long elapsedTime = System.currentTimeMillis() - startTime;
-                System.out.println("Time elapsed: " + elapsedTime + " milliseconds");
+                logger.info("Time elapsed: " + elapsedTime + " milliseconds");
 
-                // Sleep for random amount of time between 1 to 10 seconds
                 int sleepTime = new Random().nextInt(10) + 1;
-                System.out.println("Sleeping for " + sleepTime + " seconds...");
+                logger.info("Sleeping for " + sleepTime + " seconds...");
                 Thread.sleep(sleepTime * 1000);
             }
         } catch (Exception e) {
