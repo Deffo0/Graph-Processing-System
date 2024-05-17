d63103e41ac1bb3a3201e2e17d5a15bb086a5349
bazina <youssefbazina7@gmail.com>
2024-05-17 03:58:27 +0300

    Use classpath to access the locations of class path

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,11 +9,12 @@ public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         Logger logger = Logger.getLogger(Main.class.getName());
         Configuration configuration = new Configuration();
+        String classpath = System.getProperty("java.class.path");
         
         ProcessBuilder serverProcessBuilder = new ProcessBuilder(
                 "java",
                 "-cp",
-                "D:\\Library\\Faculty of Engineering\\Year 4\\2nd Semester\\Solutions\\Distributed Systems\\RMI\\target\\classes",
+                classpath,
                 "org.example.server.GSPServer",
                 configuration.getServerAddress(),
                 String.valueOf(configuration.getServerPort()),
@@ -22,46 +23,46 @@ public class Main {
         serverProcessBuilder.redirectErrorStream(true);
         Process serverProcess = serverProcessBuilder.start();
 
-        // Wait for the server process to start (optional)
-        Thread.sleep(2000); // Adjust this delay as needed
+        Thread.sleep(2000);
 
         try {
-            String[] nodeAddresses = configuration.getNodeAddresses();
-            List<Process> clientProcesses = new ArrayList<>();
+            List<Process> clientProcesses = getClientProcesses(configuration, classpath);
 
-            for (int i = 0; i < nodeAddresses.length; i++) {
-                String nodeAddress = nodeAddresses[i];
-
-                // Start the GSPClient process
-                ProcessBuilder clientProcessBuilder = new ProcessBuilder(
-                        "java",
-                        "-cp",
-                        "D:\\Library\\Faculty of Engineering\\Year 4\\2nd Semester\\Solutions\\Distributed Systems\\RMI\\target\\classes",
-                        "org.example.client.GSPClient",
-                        String.valueOf(i),
-                        nodeAddress,
-                        configuration.getServerAddress(),
-                        String.valueOf(configuration.getRmiRegistryPort()),
-                        configuration.getServerName()
-                );
-                clientProcessBuilder.redirectErrorStream(true);
-                Process clientProcess = clientProcessBuilder.start();
-                clientProcesses.add(clientProcess);
-            }
-
-            // Wait for all client processes to finish
             for (Process clientProcess : clientProcesses) {
                 clientProcess.waitFor();
             }
 
-            // Optional: Wait for the server process to finish
             serverProcess.waitFor();
-
         } catch (IOException | InterruptedException e) {
             logger.severe("An error occurred: " + e.getMessage());
         } finally {
-            // Optional: Destroy the server process
             serverProcess.destroy();
         }
+    }
+
+    private static List<Process> getClientProcesses(Configuration configuration, String classpath) throws IOException {
+        String[] nodeAddresses = configuration.getNodeAddresses();
+        List<Process> clientProcesses = new ArrayList<>();
+
+        for (int i = 0; i < nodeAddresses.length; i++) {
+            String nodeAddress = nodeAddresses[i];
+
+            ProcessBuilder clientProcessBuilder = new ProcessBuilder(
+                    "java",
+                    "-cp",
+                    classpath,
+                    "org.example.client.GSPClient",
+                    String.valueOf(i),
+                    nodeAddress,
+                    configuration.getServerAddress(),
+                    String.valueOf(configuration.getRmiRegistryPort()),
+                    configuration.getServerName()
+            );
+
+            clientProcessBuilder.redirectErrorStream(true);
+            Process clientProcess = clientProcessBuilder.start();
+            clientProcesses.add(clientProcess);
+        }
+        return clientProcesses;
     }
 }
