98f3df0163768a6ddd0c1008e0c0a6dc4007694a
Karim Alaa <karimalaa1912@gmail.com>
2024-05-18 20:33:39 +0300

    updated props

--- build.sh
+++ build.sh
@@ -4,6 +4,3 @@ mvn clean
 
 mvn install -Dname=Server -DmainClass=org.example.server.GSPServer
 docker build -t gsp-server:latest -f server.Dockerfile .
-
-# mvn install -Dname=Client -DmainClass=org.example.client.GSPClient
-# docker build -t gsp-client:latest -f client.Dockerfile .
